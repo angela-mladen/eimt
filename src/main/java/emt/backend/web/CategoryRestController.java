c2d0ee57c3777f5c13a830529b0836b224268567
angela-mladen <amladenovska001@gmail.com>
2023-03-27 19:24:37 +0200

    modified code

---
+++ src/main/java/emt/backend/web/CategoryRestController.java
@@ -0,0 +1,19 @@
+package emt.backend.web;
+
+import emt.backend.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryRestController {
+
+    @GetMapping("/list")
+    public List<Category> listCategories(){
+        return Arrays.asList(Category.values());
+    }
+}
